60a7df0daea7489bafb9f1d7e2ef49c57c932924
MrJonson <flashh@list.ru>
2025-05-11 12:59:35 +0300

    upgrade misc. add logging

--- misc.py
+++ misc.py
@@ -1,13 +1,14 @@
 from datetime import datetime
+from app import logger
 
-
-def on_start():
+def on_start(bot):
     """
     Функция, вызываемая при запуске бота.
 
     Выводит в консоль текущие дату и время начала работы бота.
     Используется как обработчик события startup в aiogram.
     """
+    logger.info(f"Бот запущен, ID: {bot.id}")
     # Получаем текущее время и форматируем его в строку
     now = datetime.now().strftime('%H:%M:%S %d/%m/%Y')
     # Выводим сообщение о запуске бота
